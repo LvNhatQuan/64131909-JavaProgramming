443a425274a833f94675dcbab625397258920194
LvNhatQuan <nhatquanb@gmail.com>
2025-02-15 14:32:14 +0700

    Hello
    Demo Hello

---
+++ Hello/src/SayHelloApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao. Toi la sinh vien khoa 64 CNTT_NTU");
+		System.out.println("Le Van Nhat Quan");
+	}
+
+}
